947f0b926b4a90c4bded35806fab9495206cd7a9
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-08-04 15:25:56 +0100

    add edit trip to admin panel feature in progress

--- src/js/admin.js
+++ src/js/admin.js
@@ -11,10 +11,11 @@ console.log("admin");
 
 async function init() {
     // load excursion view for admin
+    let excursions;
     const manageAPI = new ExcursionsAPI();
     const APIurl = "http://localhost:3000/excursions";
     try {
-        const excursions = await manageAPI.getAPI(APIurl);
+        excursions = await manageAPI.getAPI(APIurl);
         console.log(excursions);
         const markups = createTrips(excursions);
         loadTrips(markups);
@@ -22,8 +23,8 @@ async function init() {
         console.log(error);
     }
     // add new trip to the panel //
-
     /* enable adding empty trips! */
+    /* clear inputs */
 
     const addBtn = document.querySelector(".order__field-submit");
     addBtn.addEventListener("click", function (e) {
@@ -41,9 +42,70 @@ async function init() {
             deleteTrip(btn, manageAPI, APIurl);
         });
     });
+
+    // edit trips from the panel //
+    const editBtns = Array.from(
+        document.querySelectorAll(".excursions__field-input--update")
+    );
+    editBtns.forEach((btn) => {
+        btn.addEventListener("click", function (e) {
+            e.preventDefault();
+            const displayedTrip =
+                e.target.parentElement.parentElement.parentElement;
+            console.log(displayedTrip);
+            const trip = findTriptoUpdate(btn, excursions);
+            displayTripToUpdate(trip);
+            createSaveBtn();
+            const saveBtn = addBtn.nextElementSibling;
+            saveBtn.addEventListener("click", function (e) {
+                e.preventDefault();
+                // remove updated trip from data.json and display
+                const id = trip[0].id;
+                manageAPI.deleteFromAPI(APIurl, id);
+                displayedTrip.remove();
+                // get data from inputs
+                addTrip(APIurl, manageAPI);
+            });
+        });
+    });
 }
 
 // Edit any trip from the panel //
+
+// 03. po kliknieciu zapisz obiekt z inputu zastepuje obiekt ktory mial byc zedytowant
+// 04. API usuwa obiekt i dodaje nowy.
+
+function findTriptoUpdate(btn, excursions) {
+    // 01. find object with matching id
+    const parent = btn.parentElement.parentElement.parentElement;
+    const id = parent.dataset.id;
+    const trip = excursions.filter((trip) => {
+        return trip.id === +id;
+    });
+    return trip;
+}
+
+function displayTripToUpdate(trip) {
+    // 02. fill in inputs with object data
+    const title = document.querySelector(".form__field");
+    title.value = trip[0].title;
+    const description = document.querySelector(".form__field--longtext");
+    description.value = trip[0].description;
+    const adultPrice = document.querySelector("input[name=adult]");
+    adultPrice.value = trip[0].adultPrice;
+    const childPrice = document.querySelector("input[name=child]");
+    childPrice.value = trip[0].childPrice;
+}
+
+function createSaveBtn() {
+    // 02. create save button and disable add button
+    const addBtn = document.querySelector(".order__field-submit");
+    const saveBtn = addBtn.cloneNode();
+    saveBtn.value = "zapisz";
+    addBtn.disabled = true;
+    const parentBtns = addBtn.parentElement;
+    parentBtns.appendChild(saveBtn);
+}
 
 // Delete any trip from the panel //
 
@@ -56,20 +118,36 @@ function deleteTrip(btn, manageAPI, APIurl) {
 
 // Add new trip to the admin panel //
 
-function addTrip(APIurl, manageAPI) {
+async function addTrip(APIurl, manageAPI) {
     const tripData = getTripData();
+    const title = tripData.title;
+    // add trip to data.json
     manageAPI.addToAPI(APIurl, tripData);
-    const markup = createTrip(tripData);
+    // get data from data.json
+    let tripDataWithId;
+    try {
+        tripDataWithId = await manageAPI.getAPI(APIurl);
+    } catch (err) {
+        console.log(err);
+    }
+    // filter for trip with matching id
+    const matchedTrip = tripDataWithId.filter((trip) => {
+        return trip.title === title;
+    });
+    // create element with matching id
+    const markup = createTrip(matchedTrip[0]);
     const excursionsPanel = document.querySelector(".panel__excursions");
     excursionsPanel.insertAdjacentHTML("afterbegin", markup);
 }
 
 function createTrip(tripData) {
+    const id = tripData.id;
     const title = tripData.title;
     const description = tripData.description;
     const adultPrice = tripData.adultPrice;
     const childPrice = tripData.childPrice;
     const markup = createTripsMarkup(
+        id,
         title,
         description,
         adultPrice,
